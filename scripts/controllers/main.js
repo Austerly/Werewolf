c2777f012ac21e1e5775209ce30b9a4b79078ad8
Austin Hesterly <austin.hesterly@gmail.com>
2014-07-22 20:20:49 -0600

    Combined views in form.html

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -7,13 +7,7 @@ angular.module('werewolfApp').controller('MainCtrl',
 	$scope.scender = "cows";
 	$scope.gameTitleInput;
 	$scope.gameTitle = "What game are we playing today?";
-	$scope.totalCards = 0;
-	$scope.currentCard;
-	// $scope.sorter = 'name';
-	// $.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(myJsonDataFromServer){
-	// 	$scope.friends = myJsonDataFromServer.results;
-	// 	$scope.$apply(function(){
-	// 		$scope.friends = my.results;
-	// 	});
-	// });
-		});
+	$scope.totalCards = 1;
+	$scope.currentCard = 1;
+	$scope.cardsInDeck;
+});
